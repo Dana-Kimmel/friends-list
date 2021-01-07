0187c1e3c50f1e2129ed50519ad68991f616c8ba
Dana-Kimmel <dear.prudence34@gmail.com>
2021-01-07 15:00:38 -0500

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'friends-list';
+  // create an array named friends and populate with some friends
+  friends = ["Sean", "Phil", "Julie", "Ron"];
 }
